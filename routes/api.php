3dc06981388d843ea57096e4aac6c33a4359e22b
nono <nonokub.671@gmail.com>
2024-09-25 22:04:06 +0700

    refactor api

--- routes/api.php
+++ routes/api.php
@@ -12,21 +12,3 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/products/category/{category}', [ProductController::class, 'getByCategory']);
-
-Route::middleware('auth:api')->group(function () {
-
-    Route::get('/wishlist', [WishlistController::class, 'getWishlistProducts']);
-    Route::post('/wishlist/{product}', [WishlistController::class, 'addWishlistProduct']);
-
-    Route::get('/cart', [CartController::class, 'getCartProducts']);
-    Route::post('/cart/{product}', [CartController::class, 'addCartProduct']);
-    Route::put('/cart/{product}', [CartController::class, 'updateCartItem']);
-    Route::delete('/cart/{product}', [CartController::class, 'deleteCartItem']);
-
-    Route::post('/purchase/{product}', [SalesLogController::class, 'addPurchase']);
-
-    Route::get('/products/{product}/reviews', [ReviewController::class, 'getProductReviews']);
-    Route::post('/products/{product}/reviews', [ReviewController::class, 'addProductReview']);
-});
-
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,26 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/products/category/{category}', [ProductController::class, 'getByCategory']);
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/wishlist', [WishlistController::class, 'getWishlistProducts']);
+    Route::post('/wishlist/{product}', [WishlistController::class, 'addWishlistProduct']);
+
+    Route::get('/cart', [CartController::class, 'getCartProducts']);
+    Route::post('/cart/{product}', [CartController::class, 'addCartProduct']);
+    Route::put('/cart/{product}', [CartController::class, 'updateCartItem']);
+    Route::delete('/cart/{product}', [CartController::class, 'deleteCartItem']);
+
+    Route::post('/purchase/{product}', [SalesLogController::class, 'addPurchase']);
+
+    Route::get('/products/{product}/reviews', [RatingController::class, 'getProductReviews']);
+    Route::post('/products/{product}/reviews', [RatingController::class, 'addProductReview']);
+
 });
 
 require __DIR__.'/auth.php';
