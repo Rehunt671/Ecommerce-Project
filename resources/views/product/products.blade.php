235c098f10ddb4a316f37ecd59d980acce355ce4
nono <nonokub.671@gmail.com>
2024-09-28 14:28:24 +0700

    save before diaster

--- resources/views/product/products.blade.php
+++ resources/views/product/products.blade.php
@@ -1,22 +1,27 @@
 <x-app-layout>
     <section class="my-10 text-center">
         <h1>{{ $category->name }}</h1>
-        <div class="flex justify-center items-center bg-gray-200 rounded-full mx-auto w-1/2">
-            <input type="text" placeholder="Search" class="p-2 w-full outline-none rounded-l-full bg-transparent" />
-            <button class="p-2">
-                <img src="{{ asset('/storage/search-icon.png') }}" alt="Search" />
-            </button>
+        <div class="flex items-center justify-center">
+            <x-search-bar />
         </div>
     </section>
 
     <section>
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 md:gap-6 lg:gap-10 justify-items-center">
-                @foreach($products as $product)
-            <div>
-                <x-product-card :product="$product" />
+        <div class="flex justify-between">
+            <div class=" md:block w-1/6"> <!-- Left Section -->
+                <!-- Add any content you want in the left section -->
             </div>
-        @endforeach
-        </div>  
+
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 md:gap-6 lg:gap-10 justify-items-center mx-10">
+                @foreach($products as $product)
+                        <x-product-card :product="$product" />
+                @endforeach
+            </div>
+
+            <div class=" md:block w-1/6"> <!-- Right Section -->
+                <!-- Add any content you want in the right section -->
+            </div>
+        </div>
         <!-- Pagination Links -->
         <div class="mt-4">
             {{ $products->links() }} 
