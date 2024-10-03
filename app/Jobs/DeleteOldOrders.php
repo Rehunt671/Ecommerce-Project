140829be6f4420fc5717d24be5217a13de46e66b
fdsdsfdsf <NestZ671@gmail.com>
2024-10-03 14:12:54 +0700

    cronjob + buy immediate func

---
+++ app/Jobs/DeleteOldOrders.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Support\Facades\DB;
+
+class DeleteOldOrders
+{
+    public function __invoke()
+    {
+        DB::table('orders')
+        ->where('created_at', '<', now()->subDays(7))
+        ->whereNull('purchase_date')
+        ->delete();
+    }
+}
